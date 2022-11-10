b7907d0332e0903551e48f57bbd1d34f45a05bcc
AMasetti <augmas15@gmail.com>
2022-11-10 13:41:11 -0300

    initial files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # EKO-Telegram-Soil-Sensor
 This project uses an ESP8266 and an analog Capacitive Soil Sensor to alert a user of it's plant health
+
+# Requirements 
+This repo uses the [universal-telegram-bot-library](https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot) as a basis for sending messages using a Telegram Bot, need to have that installed first.
+
+Also a Telegram Bot is needed, for that we use Telegram and by messaging [BotFather](https://telegram.me/botfather) with the `/newbot` command. From there we can name our bot and add a profile picture for it. After that BotFather will give us the Access Token for that Bot in the following form `XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`. Take note of it and do not share it with anyone.
+
+For our own Telegram user we need to grab our ID, and for that we message [UserInfoBot](https://telegram.me/userinfobot) with the `/start` command. Save the 9-Digit ID for later
+
+flowchart LR
+
+A[Start] --> B(Connect To WiFi, configure and send startup alert)
+B --> C{Soil Hum Level}
+C -->|hum < 600| D[Too much water notification]
+C -->|600 < hum < 1000| E[Normal humidity level notification]
+C -->|hum > 1000| F[Dehidration notification]
---
+++ src/EKO-Telegram-Simple/EKO-Telegram-Simple.ino
@@ -0,0 +1,87 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+// Configuraciones de WiFi
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PASSWORD "YOUR_SSID_PASS"
+// Configuraciones de Telegram BOT
+#define BOT_TOKEN "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#define CHAT_ID "123456789"
+
+X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+WiFiClientSecure secured_client;
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  // Nos conectamos a la red Wifi
+  Serial.print("Connecting to Wifi SSID ");
+  Serial.print(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  secured_client.setTrustAnchors(&cert);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("\nWiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Retrieving time: ");
+  configTime(0, 0, "pool.ntp.org"); // get UTC time via NTP
+  time_t now = time(nullptr);
+  while (now < 24 * 3600)
+  {
+    Serial.print(".");
+    delay(100);
+    now = time(nullptr);
+  }
+  Serial.println(now);
+
+  int humedad = analogRead(A0);
+
+  Serial.println(humedad);
+
+  bot.sendMessage(CHAT_ID, "EKO Bot arrancó con exito 🪴", "");
+
+  bot.sendMessage(CHAT_ID, "Viendo que tal esta el suelo 👀", "");
+  
+}
+
+int alerta_enviada = 0;
+int startup = 1;
+
+void loop() {
+
+  // Leemos la humedad de suelo desde el pin analogico (rango de 0 a 1024 siendo <600 sumergido en agua)
+  int humedad = analogRead(A0);
+  Serial.println(humedad);
+
+  // Regamos con mucha agua
+  if ( humedad < 700 &&  (!alerta_enviada || startup)) {
+    alerta_enviada = 1 ;
+    startup = 0;
+    bot.sendMessage(CHAT_ID, "Me regaste de más, me voy a morir 🥺", "");
+  }
+
+  // La tierra esta seca
+  if ( humedad > 1000 && (!alerta_enviada || startup)) {
+    alerta_enviada = 1 ;
+    startup = 0;
+    bot.sendMessage(CHAT_ID, "Regame por favor 🙏", "");
+  }
+  
+
+  // Rango normal de humedad, solo alertamos si previamente no lo hicimos
+  if ( humedad > 700 && humedad < 1000 && (alerta_enviada || startup)) {
+    alerta_enviada = 0 ;
+    startup = 0;
+    bot.sendMessage(CHAT_ID, "Todo piola, gracias por cuidarme 🙌", "");
+  }
+
+  delay(1000);
+}
